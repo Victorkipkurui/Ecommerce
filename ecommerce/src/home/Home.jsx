140a1fbf355f7315da045dfcc401ae7d50fa211f
Victor Kipkurui <kipkurui.victor18@students.dkut.ac.ke>
2024-09-21 14:27:20 +0300

    map

--- ecommerce/src/home/Home.jsx
+++ ecommerce/src/home/Home.jsx
@@ -3,6 +3,8 @@ import Banner from './Banner'
 import HomeCategory from './HomeCategory'
 import CategoryShowcase from './categoryShowcase'
 import Register from '../components/Register'
+import Location from './Location'
+import AboutUs from './AboutUs'
 
 const Home=()=> {
   return (
@@ -11,6 +13,8 @@ const Home=()=> {
       <HomeCategory></HomeCategory>
       <CategoryShowcase></CategoryShowcase>
       <Register></Register>
+      <Location></Location>
+      <AboutUs></AboutUs>
     </div>
   )
 }
